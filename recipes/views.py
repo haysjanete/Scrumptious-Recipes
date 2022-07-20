f093c87dbb627e5c9edd6c087588eb5748b408e0
Janete <martinezjanete8@gmail.com>
2022-07-20 18:38:40 -0500

    Log in and logout now work

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,9 @@ from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
-
 from recipes.forms import RatingForm
-
-
-from recipes.forms import RecipeForm
 from recipes.models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def log_rating(request, recipe_id):
@@ -40,21 +37,21 @@ class RecipeDetailView(DetailView):
         return context
 
 
-class RecipeCreateView(CreateView):
+class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
     template_name = "recipes/new.html"
     fields = ["name", "author", "description", "image"]
     success_url = reverse_lazy("recipes_list")
 
 
-class RecipeUpdateView(UpdateView):
+class RecipeUpdateView(LoginRequiredMixin, UpdateView):
     model = Recipe
     template_name = "recipes/edit.html"
     fields = ["name", "author", "description", "image"]
     success_url = reverse_lazy("recipes_list")
 
 
-class RecipeDeleteView(DeleteView):
+class RecipeDeleteView(LoginRequiredMixin, DeleteView):
     model = Recipe
     template_name = "recipes/delete.html"
     success_url = reverse_lazy("recipes_list")
